04837f2f2da28a3ca04d3b8513e0f14bcf1c25af
Tammo0987 <tammo.steffens@googlemail.com>
2019-10-28 21:54:50 +0100

    Correct merge conflicts

--- src/main/kotlin/app/fiber/Gateway.kt
+++ src/main/kotlin/app/fiber/Gateway.kt
@@ -9,8 +9,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.receive
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.TextContent
+import io.ktor.http.withCharset
 import io.ktor.request.uri
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.route
@@ -18,10 +23,12 @@ import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.koin.dsl.module
 import org.koin.ktor.ext.Koin
 import org.koin.ktor.ext.inject
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.lang.Exception
 
 fun main() {
     val client = HttpClient()
@@ -29,6 +36,21 @@ fun main() {
     embeddedServer(Netty, 8080) {
         install(DefaultHeaders)
         install(CallLogging)
+
+        install(StatusPages) {
+            exception<Exception> {
+                val writer = StringWriter()
+                it.printStackTrace(PrintWriter(writer))
+
+                call.respond(
+                    TextContent(
+                        writer.toString(),
+                        ContentType.Text.Plain.withCharset(Charsets.UTF_8),
+                        HttpStatusCode.InternalServerError
+                    )
+                )
+            }
+        }
 
         install(Koin) {
             modules(gatewayModule)
