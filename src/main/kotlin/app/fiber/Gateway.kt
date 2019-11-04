d12f3de0a8f2ef4a6467a1769bb69c08cd03ce14
Tammo0987 <tammo.steffens@googlemail.com>
2019-11-04 21:45:46 +0100

    Removed redundant naming 2

--- src/main/kotlin/app/fiber/Gateway.kt
+++ src/main/kotlin/app/fiber/Gateway.kt
@@ -1,40 +1,49 @@
 package app.fiber
 
+import app.fiber.feature.Forwarding
 import app.fiber.service.ServiceRepository
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.StatusPages
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.TextContent
 import io.ktor.http.withCharset
 import io.ktor.response.respond
 import io.ktor.server.engine.commandLineEnvironment
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import org.koin.dsl.module
+import org.koin.ktor.ext.Koin
+import java.io.PrintWriter
+import java.io.StringWriter
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty, commandLineEnvironment(args)){}.start(true)
+    embeddedServer(Netty, commandLineEnvironment(args)).start(true)
 }
 
 fun Application.main() {
-    install(org.koin.ktor.ext.Koin) {
+    install(Koin) {
         modules(gatewayModule)
     }
 
-    install(io.ktor.features.DefaultHeaders)
-    install(io.ktor.features.CallLogging)
-    install(app.fiber.feature.Forwarding)
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Forwarding)
 
-    install(io.ktor.features.StatusPages) {
+    install(StatusPages) {
         exception<Exception> {
-            val writer = java.io.StringWriter()
-            it.printStackTrace(java.io.PrintWriter(writer))
+            val writer = StringWriter()
+            it.printStackTrace(PrintWriter(writer))
 
             call.respond(
-                io.ktor.http.content.TextContent(
+                TextContent(
                     writer.toString(),
                     ContentType.Text.Plain.withCharset(Charsets.UTF_8),
-                    io.ktor.http.HttpStatusCode.InternalServerError
+                    HttpStatusCode.InternalServerError
                 )
             )
         }
